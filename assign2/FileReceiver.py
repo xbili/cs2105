d7c7a7d38aeb31064dcbd66f547ed07ac1908b20
Bili Xu <xubili93@gmail.com>
2015-10-09 13:11:55 +0800

    Remove global namespaced socket module methods

--- assign2/FileReceiver.py
+++ assign2/FileReceiver.py
@@ -1,17 +1,18 @@
 import os, sys
-from socket import *
+import socket
 
 server_name = 'localhost'
 server_port = 9001
 
 def main():
-    rcv_sock = socket(AF_INET, SOCK_DGRAM)
-    socket.bind(rcv_sock, ('', server_port))
+    rcv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    rcv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    rcv_sock.bind(('', server_port))
     print 'Receiving file on', server_port
     while True:
         message, client_address = rcv_sock.recvfrom(2048)
         print message
-    socket.close(rcv_sock)
+    rcv_sock.close()
 
 if __name__ == '__main__':
     main()
