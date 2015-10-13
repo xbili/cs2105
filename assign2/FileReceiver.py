4241180b72d3e58683527093db8aaa7fdcdaa7e7
Bili Xu <xubili93@gmail.com>
2015-10-14 00:42:41 +0800

    Include header with ACK and Seq number in packet

--- assign2/FileReceiver.py
+++ assign2/FileReceiver.py
@@ -1,19 +1,45 @@
 import os, sys
 import socket
+import pickle
 
 server_name = 'localhost'
 server_port = 9001
 
-def main():
-    rcv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    rcv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    rcv_sock.bind(('', server_port))
+class Packet:
+    def __init__(self, seqnum, acknum, checksum, payload):
+        self.seqnum = seqnum # 0 / 1
+        self.acknum = acknum # 0 / 1
+        self.checksum = checksum # Binary string
+        self.payload = payload
 
-    print 'Receiving file on', server_port
+class Receiver:
+    def __init__(self):
+        self.rcv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.rcv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.rcv_sock.bind(('', server_port))
+        print 'Receiving file on', server_port
+
+    def _recv(self, size):
+        return self.rcv_sock.recvfrom(size)
+
+    def _close(self):
+        self.rcv_sock.close()
+
+def main():
+    rcv = Receiver()
+
+    # Writes into the new file
+    f = open('large1.mp4', 'w')
+
+    count = 1
+
     while True:
-        message, client_address = rcv_sock.recvfrom(2048)
-        print message
-    rcv_sock.close()
+        pkt_string, client_address = rcv._recv(2048)
+        print 'Writing message', count
+        pkt = pickle.loads(pkt_string)
+        f.write(pkt.payload)
+        count+=1
+    rcv._close()
 
 if __name__ == '__main__':
     main()
--- assign2/FileSender.py
+++ assign2/FileSender.py
@@ -1,22 +1,70 @@
 import os, sys
 import socket
+import pickle
+import time
 
 server_name = 'localhost'
 server_port = 9000
 
+class Message:
+    def __init__(self, data):
+        # Data is an array of 20 char
+        self.data = data
+
+class Packet:
+    def __init__(self, seqnum, acknum, checksum, payload):
+        self.seqnum = seqnum # 0 / 1
+        self.acknum = acknum # 0 / 1
+        self.checksum = checksum # Binary string
+        self.payload = payload
+
+class Sender:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.connect((server_name, server_port))
+
+    def _output(self, msg):
+        self.sock.send(msg)
+
+    def _close(self):
+        self.sock.close()
+
+def toggle_bit(bit):
+    if bit == 1:
+        return 0
+    if bit == 0:
+        return 1
+
 def main():
-    sender_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sender_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sender_sock.connect((server_name, server_port))
+    start = time.time()
+    sender = Sender()
 
+    seq_num = 0
+    ack_num = 0
     data = open(sys.argv[1], 'r')
-    pkt = data.read(1024)
-    while (pkt):
-        print 'Sending'
-        sender_sock.send(pkt)
-        pkt = data.read(1024)
+    payload = data.read(200)
+    pkt = Packet(seq_num, ack_num, '0000000000000000', payload)
+    pkt = pickle.dumps(pkt)
 
-    sender_sock.close()
+    while payload:
+        seq_num = toggle_bit(seq_num)
+        ack_num = toggle_bit(ack_num)
+
+        # For debug
+        print 'Sending'
+        print '---'
+        print 'seq_num:', seq_num
+        print 'ack_num:', ack_num
+        print '---'
+
+        pkt = Packet(seq_num, ack_num, '0000000000000000', payload)
+        pkt = pickle.dumps(pkt)
+        sender._output(pkt)
+        payload = data.read(200)
+
+    sender._close()
+    print time.time() - start
 
 if __name__ == '__main__':
     main()
